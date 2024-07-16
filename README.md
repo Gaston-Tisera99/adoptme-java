a0bb9c2a734fb09e8d60c6f20958920594bc66a2
Gaston-Tisera99 <gastontisera94@gmail.com>
2024-07-16 20:44:09 -0300

    services

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # adoptme-java
-Tienda de Perros
+Se creo las entidades Pet y User. 
+Luego se crearon los repositorios correspondientes a las entidades para acceder a esas entidades
+Se crearon los servicios para gestionar las diferentes solicitudes de cada entidad.
--- src/main/java/com/adoptme/petshop/services/PetsService.java
+++ src/main/java/com/adoptme/petshop/services/PetsService.java
@@ -1,18 +1,29 @@
 package com.adoptme.petshop.services;
 
+import com.adoptme.petshop.entities.Pet;
+import com.adoptme.petshop.repositories.PetsRepository;
+import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PetsService {
 
+    @Autowired private PetsRepository repository;
 
-    public User save(User user){
-        return repository.save(user);
+    public Pet save(Pet pet){
+        return repository.save(pet);
     }
 
-    public List<User> readAll(){}
+    public List<Pet> readAll(){
+        return repository.findAll();
+    }
 
-    public Optional<User> readOne(Long id){
-
+    public Optional<Pet> readOne(Long id){
+        return repository.findById(id);
     }
 
     public void destroyOne(Long id){
